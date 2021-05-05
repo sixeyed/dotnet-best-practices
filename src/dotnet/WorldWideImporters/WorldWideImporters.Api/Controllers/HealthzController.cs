0344f846b7e56531bc8d5dc82f1b361b058799cb
Elton Stoneman <elton@sixeyed.com>
2021-05-05 11:36:26 +0100

    Add Kubernetes

---
+++ src/dotnet/WorldWideImporters/WorldWideImporters.Api/Controllers/HealthzController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WorldWideImporters.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HealthzController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
